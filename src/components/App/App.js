c05af85c8443071eb01e1f80991bf71226f16a37
Eric Chaves <erchaves@gmail.com>
2015-05-23 09:11:58 -0700

    removing header, footer, and pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,16 +6,10 @@ import withContext from '../../decorators/withContext'; // eslint-disable-line n
 import withStyles from '../../decorators/withStyles'; // eslint-disable-line no-unused-vars
 import AppActions from '../../actions/AppActions';
 import AppStore from '../../stores/AppStore';
-import Header from '../Header';
 import ContentPage from '../ContentPage';
-import ContactPage from '../ContactPage';
-import LoginPage from '../LoginPage';
-import RegisterPage from '../RegisterPage';
 import NotFoundPage from '../NotFoundPage';
-import Feedback from '../Feedback';
-import Footer from '../Footer';
 
-const pages = { ContentPage, ContactPage, LoginPage, RegisterPage, NotFoundPage };
+const pages = { ContentPage };
 
 @withContext
 @withStyles(styles)
@@ -43,31 +37,19 @@ class App {
     switch (this.props.path) {
 
       case '/':
-      case '/about':
-      case '/privacy':
+      // case '/someOtherPath':
         let page = AppStore.getPage(this.props.path);
         component = React.createElement(pages[page.component], page);
         break;
 
-      case '/contact':
-        component = <ContactPage />;
-        break;
-
-      case '/login':
-        component = <LoginPage />;
-        break;
-
-      case '/register':
-        component = <RegisterPage />;
-        break;
+      // case '/someComponent':
+      //   component = <SomeComponent />;
+      //   break;
     }
 
     return component ? (
       <div>
-        <Header />
         {component}
-        <Feedback />
-        <Footer />
       </div>
     ) : <NotFoundPage />;
   }
